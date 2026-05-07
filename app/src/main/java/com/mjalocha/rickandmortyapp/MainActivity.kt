3d494b62ed2a232dc48a576ee10bdf14d6edc2cf
mikolaj-jalocha <mikics2k17@gmail.com>
2026-05-07 14:55:19 +0200

    init: initial commit

---
+++ app/src/main/java/com/mjalocha/rickandmortyapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mjalocha.rickandmortyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.mjalocha.rickandmortyapp.ui.theme.RickAndMortyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RickAndMortyAppTheme {
+
+            }
+        }
+    }
+}

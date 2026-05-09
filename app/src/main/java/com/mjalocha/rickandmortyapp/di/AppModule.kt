8b8d787bb64979012cef3028fbeff5debf4c49a2
mikolaj-jalocha <mikics2k17@gmail.com>
2026-05-09 18:20:42 +0200

    feat: integrate DI

--- app/src/main/java/com/mjalocha/rickandmortyapp/di/AppModule.kt
+++ app/src/main/java/com/mjalocha/rickandmortyapp/di/AppModule.kt
@@ -1,5 +1,10 @@
 package com.mjalocha.rickandmortyapp.di
 
+import com.mjalocha.rickandmortyapp.data.network.RickAndMortyApi
+import com.mjalocha.rickandmortyapp.data.network.RickAndMortyApiImpl
+import com.mjalocha.rickandmortyapp.data.repository.CharacterRepository
+import com.mjalocha.rickandmortyapp.data.repository.CharacterRepositoryImpl
+import com.mjalocha.rickandmortyapp.ui.characters_screen.CharactersViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -10,6 +15,7 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.KoinViewModel
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
@@ -30,5 +36,15 @@ class AppModule {
         }
     }
 
+    @Single(binds = [RickAndMortyApi::class])
+    fun ktorDataSource(httpClient: HttpClient) = RickAndMortyApiImpl(httpClient)
+
+    @Single(binds = [CharacterRepository::class])
+    fun charactersRepository(apiClient: RickAndMortyApi) = CharacterRepositoryImpl(apiClient)
+
+    @KoinViewModel
+    fun charactersViewModel(
+        characterRepository: CharacterRepository
+    ) = CharactersViewModel(characterRepository)
 
 }

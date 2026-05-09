57083a26a3df6e9d22f704da2fe97e308792e497
mikolaj-jalocha <mikics2k17@gmail.com>
2026-05-09 18:20:08 +0200

    feat: setup api client

---
+++ app/src/main/java/com/mjalocha/rickandmortyapp/data/network/RickAndMortyApiImpl.kt
@@ -0,0 +1,35 @@
+package com.mjalocha.rickandmortyapp.data.network
+
+import com.mjalocha.rickandmortyapp.data.model.CharacterResponse
+import com.mjalocha.rickandmortyapp.data.model.Gender
+import com.mjalocha.rickandmortyapp.data.model.Status
+import com.mjalocha.rickandmortyapp.data.utils.DataError
+import com.mjalocha.rickandmortyapp.data.utils.Result
+import com.mjalocha.rickandmortyapp.data.utils.safeCall
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+
+class RickAndMortyApiImpl(
+    private val httpClient: HttpClient
+) : RickAndMortyApi {
+    override suspend fun getCharacters(
+        page: Int,
+        status: Status?,
+        gender: Gender?
+    ): Result<CharacterResponse, DataError.Remote> {
+        return safeCall<CharacterResponse> {
+            httpClient.get {
+                url("https://rickandmortyapi.com/api/character")
+                parameter("page", page)
+                if (status != null) {
+                    parameter("status", status.name.lowercase())
+                }
+                if (gender != null) {
+                    parameter("gender", gender.name.lowercase())
+                }
+            }
+        }
+    }
+}

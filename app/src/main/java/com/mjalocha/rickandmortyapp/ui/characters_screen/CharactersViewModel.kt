6eac4328ca190e5d4dab55de5262fd476fbb014a
mikolaj-jalocha <mikics2k17@gmail.com>
2026-05-10 09:45:30 +0200

    feat: fetching content in VM

--- app/src/main/java/com/mjalocha/rickandmortyapp/ui/characters_screen/CharactersViewModel.kt
+++ app/src/main/java/com/mjalocha/rickandmortyapp/ui/characters_screen/CharactersViewModel.kt
@@ -1,10 +1,63 @@
 package com.mjalocha.rickandmortyapp.ui.characters_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mjalocha.rickandmortyapp.data.model.dto.CharacterDto
 import com.mjalocha.rickandmortyapp.data.repository.CharacterRepository
+import com.mjalocha.rickandmortyapp.data.utils.onError
+import com.mjalocha.rickandmortyapp.data.utils.onSuccess
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class CharactersViewModel(
-    private val characterRepository: CharacterRepository
+    private val repository: CharacterRepository
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow(CharactersScreenState())
+    val state: StateFlow<CharactersScreenState> = _state
+
+    private var charactersFetchJob: Job? = null
+
+    init {
+        fetchCharacters()
+    }
+
+    fun fetchCharacters() {
+        _state.update {
+            it.copy(
+                isLoading = true
+            )
+        }
+
+        charactersFetchJob?.cancel()
+        charactersFetchJob = viewModelScope.launch(Dispatchers.IO) {
+            repository.getCharacters().onSuccess { data ->
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        errorMessage = null,
+                        characters = data.results
+                    )
+                }
+            }.onError { error ->
+                _state.update {
+                    it.copy(
+                        errorMessage = error.name,
+                        isLoading = false
+                    )
+                }
+            }
+        }
+    }
 }
+
+// TODO: replace DTO with domain model
+data class CharactersScreenState(
+    val characters: List<CharacterDto> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
